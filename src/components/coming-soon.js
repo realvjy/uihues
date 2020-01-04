1519eb70760cd51a9b4784d084985fd7615d36ca
realvjy <realvjy@gmail.com>
2020-01-04 20:38:27 +0530

    added figma link

--- src/components/coming-soon.js
+++ src/components/coming-soon.js
@@ -13,11 +13,11 @@ const Soon = () => {
             <p>
               A collection of handcrafted <strong>colors</strong> and <strong>gradients</strong> for designers and developer. Easily accesible from all design tools: Figma, Sketch, XD and more.
             </p>
-            <h3>SUMMER 2020</h3>
+            <h3>COMING SUMMER 2020</h3>
+
+            <a href="https://www.figma.com/file/s5i8cCPx63EUeIwAJGzJ8b/uihues?node-id=0%3A1" className="figma" >Open Figma File</a><a href="https://instagram.com/uihues" className="insta" >Instagram</a>
             <div className="credit">
-              Project by
-                {` `}
-                <a href="https://vijayverma.co">vijay verma</a>
+              Project by {` `} <a href="https://vijayverma.co">vijay verma</a>
             </div>
           </div>
         </div>
